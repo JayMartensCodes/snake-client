f484e90e3d91f6ac447c1d171bf99ffa863c9aff
Jay Martens <martensjay93@gmail.com>
2020-07-28 20:31:26 -0400

    Created a handleUserInput function that listens for keypresses and for now exits if you press ctrl c

--- play.js
+++ play.js
@@ -1,3 +1,27 @@
 const { connect } = require('./client');
+const readline = require('readline');
+readline.emitKeypressEvents(process.stdin);
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function(data) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  data();
+  return stdin;
+};
+
+const handleUserInput = () => {
+  process.stdin.on('keypress', (str, key) => {
+    if (key.sequence === '\u0003') {
+      process.exit();
+    }
+  });
+};
+
 console.log('Connecting ...');
 connect();
+setupInput(handleUserInput);
