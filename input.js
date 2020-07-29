093d0d9a8621818e9c9ac4634e077cb7bdd1951f
Jay Martens <martensjay93@gmail.com>
2020-07-28 20:43:12 -0400

    Passed the connection object to input so that it could send movements commands to the server

--- input.js
+++ input.js
@@ -1,10 +1,12 @@
 const readline = require('readline');
 readline.emitKeypressEvents(process.stdin);
+let connection;
 /**
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -17,6 +19,14 @@ const handleUserInput = () => {
   process.stdin.on('keypress', (str, key) => {
     if (key.sequence === '\u0003') {
       process.exit();
+    } else if (key.name === 'w') {
+      connection.write('Move: up');
+    } else if (key.name === 's') {
+      connection.write('Move: down');
+    } else if (key.name === 'a') {
+      connection.write('Move: left');
+    } else if (key.name === 'd') {
+      connection.write('Move: right');
     }
   });
 };
--- play.js
+++ play.js
@@ -2,5 +2,5 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-connect();
-setupInput();
+let connectObject = connect();
+setupInput(connectObject);
